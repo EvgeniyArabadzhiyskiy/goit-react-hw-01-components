c90e90579172cf3682c5b5bc806369bae4ddc756
EVGENIY-12 <93032109+EvgeniyArabadzhiyskiy@users.noreply.github.com>
2022-06-20 16:18:08 +0300

    Add TransactionHistory

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import user from './Data/user.json';
 import data from './Data/data.json';
-import friends from "./Data/friends.json";
+import friends from './Data/friends.json';
+import transactions from './Data/transactions.json';
 
-import Profile from './components/Profile/Profile';
 import Container from 'components/Container/Container';
+import Profile from './components/Profile/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 
 const App = () => {
   return (
     <div>
       <Container>
-        
         <Profile
           username={user.username}
           avatar={user.avatar}
@@ -20,13 +21,12 @@ const App = () => {
           stats={user.stats}
         />
 
-        <Statistics title="Upload stats" stats={data}/>
-        <Statistics  stats={data}/>
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
 
         <FriendList allFriends={friends} />
 
-
-
+        <TransactionHistory transactions={transactions} />
       </Container>
     </div>
   );
---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import stl from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ transactions }) => {
+  return (
+    <table className={stl.transaction__history}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(({ id, type, amount, currency }) => {
+          return (
+            <tr className={stl.transaction__row} key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default TransactionHistory;
